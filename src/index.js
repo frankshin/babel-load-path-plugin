cb89e815ce7ccc12f3c38d6f5790d54c87316295
dengweixu <1103579820@qq.com>
2021-03-27 23:13:11 +0800

    fix

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
-export default function () {
+module.exports = function ({ types: t }) {
   return {
     visitor: {
-      Identifier(path) {
-        const { name } = path.node;
-        // reverse the name: JavaScript -> tpircSavaJ
-        path.node.name = name
-          .split('')
-          .reverse()
-          .join('');
+      // Identifier(path, state) {},
+      // ASTNodeTypeHere(path, state) {},
+      // BinaryExpression(path) {},
+      ImportDeclaration(path, state) {
+        const { node } = path;
+        let str;
+        if (!node) return;
+        if (node.source.value.indexOf('allsaberjs') > 0) {
+          node.specifiers.forEach((item) => {
+            if (t.isImportSpecifier(item)) {
+              // todo: 暂时写死，后续抽时间动态化～～
+              const { name } = item.local;
+              switch (name) {
+                case 'getCookie':
+                  str = t.identifier('allsaberjs/libs/get-cookie');
+                  break;
+                case 'setCookie':
+                  str = t.identifier('allsaberjs/libs/set-cookie');
+                  break;
+                default:
+                  str = null;
+              }
+              if (str) node.source.value = str;
+            }
+          });
+        }
       },
     },
   };
-}
+};
